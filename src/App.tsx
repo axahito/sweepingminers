016f4d37f2fee02f1ce9d472bd25e8665f521712
Chris <abiyyuakahfi@gmail.com>
2025-05-25 19:01:51 +0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import GameWindow from "./components/Game/GameWindow";
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-50 flex flex-col items-center justify-center p-4">
-      <main className="w-full h-full bg-white rounded-lg p-6 flex justify-center items-center">
+    <div className="h-screen w-screen bg-white flex flex-col items-center justify-center p-4">
+      <main className="w-full h-full  rounded-lg p-6 flex justify-center items-center">
         <GameWindow />
       </main>
+
+      <footer className="w-full h-[64px] text-sm text-black flex justify-center items-center gap-[8px]">
+        Crafted by Abiyyu Rohman <span>|</span>
+        <a href="https://www.linkedin.com/in/abiyyu-rohman-227982181/" target="_blank">
+          #100ProjectsForJapan
+        </a>
+      </footer>
     </div>
   );
 }
